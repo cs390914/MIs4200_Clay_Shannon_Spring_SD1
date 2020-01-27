da25fc8eeef4fb993fcaf21b09a265a141e811da
Clay Shannon <cs390914@ohio.edu>
2020-01-27 10:28:47 -0500

    began SD2 added 2 classes but need to comfirm my relationship before going any further

---
+++ Models/Client.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MIs4200_Clay_Shannon_Spring_SD1.Models
+{
+    public class Client
+    {
+        public int ClientID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public int CurrentWeight { get; set; }
+        public int GoalWeight { get; set; }
+        public DateTime ClientSince { get; set; }
+        public ICollection<TrainingSessions> Trainingsessions { get; set; }
+
+    }
+}
---
+++ Models/TrainingSessions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MIs4200_Clay_Shannon_Spring_SD1.Models
+{
+    public class TrainingSessions
+    {
+        public int TrainingSessionID { get; set; }
+        public string Description { get; set; }
+        public int TrainingTime { get; set; }
+        public string Equipment { get; set; }
+        public string TrainingGoal { get; set; }
+        public DateTime TrainingSessionDate { get; set; }
+    }
+}
