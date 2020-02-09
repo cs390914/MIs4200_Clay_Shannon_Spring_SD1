db32d5f7e2a431fca616cf2e33f645f31d4cca05
Clay Shannon <cs390914@ohio.edu>
2020-02-09 17:09:19 -0500

    corrected connectionstring still throwing error

---
+++ Models/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MIs4200_Clay_Shannon_Spring_SD1.Models
+{
+    public class Car
+    {
+        public int CarID { get; set; }
+        public string LicensePlate { get; set; }
+        public string NickName { get; set; }
+        public string Model { get; set; }
+        public string Brand { get; set; }
+        public DateTime PurchaseDate { get; set; }
+    }
+}
